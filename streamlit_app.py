9040ad78b69f2ecd60d6126e5d6f1f77e676b438
ysoydaniel <danielcashij@gmail.com>
2026-03-14 23:29:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,12 +177,12 @@ div[data-baseweb="select"] > div:hover {
     border-radius: 14px !important;
     color: #0f172a !important;
     border: 0px solid rgba(0,199,61,0.14) !important;
-    box-shadow: 0 0px 12px rgba(15, 23, 42, 0.04) !important;
+    box-shadow: 0 0px 0px rgba(15, 23, 42, 0.04) !important;
 }
 
 div[data-testid="stTextInput"] input:focus {
     border: 0px solid rgba(0,199,61,0.35) !important;
-    box-shadow: 0 0 0 2px rgba(0,199,61,0.08) !important;
+    box-shadow: 0 0 0 0px rgba(0,199,61,0.08) !important;
     outline: none !important;
 }
 
