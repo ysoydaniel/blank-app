3beceda35d5517383ecfea86d34b816c2be13ba1
ysoydaniel <danielcashij@gmail.com>
2026-03-14 19:56:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ st.markdown("""
     /* Botón principal */
     .stButton > button {
         width: 100%;
+        max-width: 320px;
         background: linear-gradient(135deg, #00c73d 70%, #8fe000 100%);
         color: white;
         border: none;
